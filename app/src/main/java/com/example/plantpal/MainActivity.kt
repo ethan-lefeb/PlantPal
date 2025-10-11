0a607c859e5947f81c5e0d9541bfdc0e6b327057
Devin McLaughlin <DRMcLaughlin@student.fullsail.edu>
2025-10-10 23:02:34 -0400

    Added different UI layout to the login and home page

--- app/src/main/java/com/example/plantpal/MainActivity.kt
+++ app/src/main/java/com/example/plantpal/MainActivity.kt
@@ -4,17 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.plantpal.ui.theme.PlantPalTheme
 
@@ -22,85 +14,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             PlantPalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppNavigation(Modifier.padding(innerPadding))
+                val navController = rememberNavController()
+
+                Scaffold { innerPadding ->
+                    AppNavGraph(
+                        navController = navController,
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
+
             }
-        }
-    }
-}
-
-@Composable
-fun AppNavigation(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = "start", modifier = modifier) {
-
-        composable("start") {
-            StartScreen(navController)
-        }
-
-        composable("login") {
-            LoginScreen(
-                onSuccess = {
-                    navController.navigate("home") {
-                        popUpTo("login") { inclusive = true }
-                    }
-                },
-                onNavigateToSignup = {
-                    navController.navigate("signup")
-                }
-            )
-        }
-
-
-        composable("signup") {
-            AccountCreationScreen(
-                onSuccess = {
-                    navController.navigate("home") {
-                        popUpTo("signup") { inclusive = true }
-                    }
-                },
-                onNavigateToLogin = {
-                    navController.navigate("login")
-                }
-            )
-        }
-
-        composable("home") {
-            PlantPalApp()
-        }
-    }
-}
-
-@Composable
-fun StartScreen(navController: NavHostController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Welcome to PlantPal!", modifier = Modifier.padding(bottom = 24.dp))
-
-        Button(
-            onClick = { navController.navigate("login") },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Go to Login")
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = { navController.navigate("signup") },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Create Account")
         }
     }
 }
